23e6af7482a44ff07193bfb41019a0efb502301c
Lotino Ron Jefferson <lotino@barnet.co.jp>
2022-03-31 18:01:51 +0900

    modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
+  root 'mains#home'
 
   get 'bag', to: 'categories#bag'
   get 'wallet', to: 'categories#wallet'
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'no_brand_j', to: 'categories#no_brand_j'
   get 'watch', to: 'categories#watch'
 
+  get 'buy', to: 'pages#buy'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'event', to: 'pages#event'
