8c21f6f2282074a75f5279d9fc176536dcf6eccc
Lotino Ron Jefferson <lotino@barnet.co.jp>
2022-03-31 16:22:16 +0900

    added routes other than root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
-  
+
+  get 'bag', to: 'categories#bag'
+  get 'wallet', to: 'categories#wallet'
+  get 'accessory', to: 'categories#accessory'
+  get 'brand-j', to: 'categories#brand-j'
+  get 'no_brand-j', to: 'categories#no_brand-j'
+  get 'watch', to: 'categories#watch'
+
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'event', to: 'pages#event'
+
 end
